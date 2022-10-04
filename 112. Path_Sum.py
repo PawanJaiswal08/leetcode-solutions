dfef3a50fb0adb080655805bb72da009bf05c14f
sreejasaha19 <105979237+sreejasaha19@users.noreply.github.com>
2022-10-05 00:22:47 +0530

    Delete 112. Path_Sum.py

--- 112. Path_Sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-  def hasPathSum(self, root: TreeNode, summ: int) -> bool:
-    if not root:
-      return False
-    if root.val == summ and not root.left and not root.right:
-      return True
-    return self.hasPathSum(root.left, summ - root.val) or \
-        self.hasPathSum(root.right, summ - root.val)
