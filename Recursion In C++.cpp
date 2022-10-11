25bc79d036e33f82d8c67394d50e0e303d67652a
AmitrajeetKonch <72186433+CodingWithAmit-07@users.noreply.github.com>
2022-10-11 22:37:43 +0530

    Recursion In Cpp

---
+++ Recursion In C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int factorial(int n)
+{
+	if (n <= 1)
+	{
+		return 1;
+	}
+	return n * factorial(n - 1);
+}
+int main()
+{
+	int a;
+	cout << "Enter a no." << endl;
+	cin >> a;
+	cout << "The factorial of a is " << factorial(a) << endl;
+	return 0;
+}
