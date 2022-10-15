637014fb8d9eacab114ff07550443fdc91e6f2de
Anuj Srivastav <112340128+Anujsrivastav119@users.noreply.github.com>
2022-10-15 21:37:48 +0530

    Update 268.Missing_Number.java

--- 268.Missing_Number.java
+++ 268.Missing_Number.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int sum = (n * (n+1)) / 2;
         for(int i=0; i<n; i++) {
-            sum -= nums[i];
+            sum-=nums[i];
         }
         return sum;
     }
