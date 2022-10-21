d9e683663dc4d0825ef1070fca2a618911525d53
AsesinoAkg <anuraggupta6468star@gmail.com>
2022-10-22 00:07:25 +0530

    c++ code solution for paper cutting

---
+++ papercutting.cpp
@@ -0,0 +1,34 @@
+//shree ganeshay namah
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pi (3.141592653589)
+#define mod 1000000007
+#define ll long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+
+int main()
+{
+
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int ak,akg;
+        cin>>ak>>akg;
+        cout<<(ak/akg)*(ak/akg)<<endl;
+    }
+    return 0;
+}
